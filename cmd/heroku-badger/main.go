4c831c25c1957caa2cb9642d728144b491d7e203
Oskari Rautiainen <orautiainen@splunk.com>
2022-04-15 22:25:38 -0700

    use do update syntax

--- cmd/heroku-badger/main.go
+++ cmd/heroku-badger/main.go
@@ -111,10 +111,9 @@ func main() {
       //  	VALUES ($1, $2, $3, $4)
       //  	WHERE (NOT) EXISTS (SELECT 1 FROM status WHERE app_id=$1);`, data.App.Id, data.App.Name, data.Status, data.CreatedAt )
 			_, err = db.Exec(`
-			UPDATE status SET status=$3, last_update=$4 WHERE app_id=$1 AND last_update<=$4;
 			INSERT INTO status (app_id, app_name, status, last_update)
        	VALUES ($1, $2, $3, $4)
-       	ON CONFLICT DO NOTHING;`, data.App.Id, data.App.Name, data.Status, data.CreatedAt )
+       	ON CONFLICT DO UPDATE status SET status=$3, last_update=$4 WHERE last_update<=$4;`, data.App.Id, data.App.Name, data.Status, data.CreatedAt )
 			// _, err := db.Exec(`
 			// INSERT INTO status (app_id, app_name, status, last_update)
 			// VALUES ($1, $2, $3, $4)`, data.App.Id, data.App.Name, data.Status, data.CreatedAt )
